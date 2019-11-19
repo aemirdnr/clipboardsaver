771927a8467a2e8cc73721038e525e6842b3491a
Anıl Emir Döner <53477261+aemirdnr@users.noreply.github.com>
2019-11-20 02:09:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ namespace ClipboardSaver
 {
     static class Program
     {
-        /// <summary>
-        /// Uygulamanın ana girdi noktası.
-        /// </summary>
         [STAThread]
         static void Main()
         {
